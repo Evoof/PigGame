58c9be9cc11e6db5b0712420baed7a6a016665e7
Fikayo <demixwillex@gmail.com>
2024-05-12 19:25:56 +0100

    Pig_Game

--- starter/script.js
+++ starter/script.js
@@ -12,14 +12,28 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-// Rolling the dice
+// // Rolling the dice functionality
 
-btnRoll.addEventListener('click', function () {
-  // Generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
-  // Display dice
-  diceEl.classList.remove('hiddeen');
-  diceEl.src = `dice-${dice}.png`;
-  // Check for rolled 1: if true, switc to the next player
+// btnRoll.addEventListener('click', function () {
+//   // Generating a random dice roll
+//   const dice = Math.trunc(Math.random() * 6) + 1;
+//   console.log(dice);
+
+//   // Display dice
+//   diceEl.classList.remove('hidden');
+//   diceEl.src = `dice-${dice}.png`;
+
+//   // Check for rolled 1: if true, switch to the next player
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const btnRoll = document.querySelector('.btn--roll');
+  const diceEl = document.querySelector('.dice');
+
+  btnRoll.addEventListener('click', function () {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+  });
 });
