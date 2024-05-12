1c31565317dcc84f712bc8a18c15af364cecbda6
Fikayo <demixwillex@gmail.com>
2024-05-12 01:15:52 +0100

    Pig_Game

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('btn--new');
+const btnRoll = document.querySelector('btn--roll');
+const btnHold = document.querySelector('btn--hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+// Rolling the dice
+
+btnRoll.addEventListener('click', function () {
+  // Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  // Display dice
+  diceEl.classList.remove('hiddeen');
+  diceEl.src = `dice-${dice}.png`;
+  // Check for rolled 1: if true, switc to the next player
+});
